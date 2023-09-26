31eec25634fc35ccecb237bb15db932c9ccedd9b
maneeeel <camaragonzalezmanel@gmail.com>
2023-09-26 12:06:37 +0200

    --26/09
    Writing text del homename cambiado de
    Desarollador web a web development

--- code/home-name.js
+++ code/home-name.js
@@ -11,22 +11,22 @@ setInterval(() => {
 writingText()
 function writingText() {
     movingText.innerHTML = ''
-    setTimeout(() => {movingText.innerHTML += 'D'}, 100);
+    setTimeout(() => {movingText.innerHTML += 'W'}, 100);
     setTimeout(() => {movingText.innerHTML += 'e'}, 200);
-    setTimeout(() => {movingText.innerHTML += 's'}, 300);
-    setTimeout(() => {movingText.innerHTML += 'a'}, 400);
-    setTimeout(() => {movingText.innerHTML += 'r'}, 500);
-    setTimeout(() => {movingText.innerHTML += 'o'}, 600);
-    setTimeout(() => {movingText.innerHTML += 'l'}, 700);
-    setTimeout(() => {movingText.innerHTML += 'l'}, 800);
-    setTimeout(() => {movingText.innerHTML += 'a'}, 900);
-    setTimeout(() => {movingText.innerHTML += 'd'}, 1000);
-    setTimeout(() => {movingText.innerHTML += 'o'}, 1100);
-    setTimeout(() => {movingText.innerHTML += 'r'}, 1200);
-    setTimeout(() => {movingText.innerHTML += '-'}, 1300);
-    setTimeout(() => {movingText.innerHTML += 'W'}, 1400);
-    setTimeout(() => {movingText.innerHTML += 'e'}, 1500);
-    setTimeout(() => {movingText.innerHTML += 'b'}, 1600);
+    setTimeout(() => {movingText.innerHTML += 'b'}, 300);
+    setTimeout(() => {movingText.innerHTML += '-'}, 400);
+    setTimeout(() => {movingText.innerHTML += 'D'}, 500);
+    setTimeout(() => {movingText.innerHTML += 'e'}, 600);
+    setTimeout(() => {movingText.innerHTML += 'v'}, 700);
+    setTimeout(() => {movingText.innerHTML += 'e'}, 800);
+    setTimeout(() => {movingText.innerHTML += 'l'}, 900);
+    setTimeout(() => {movingText.innerHTML += 'o'}, 1000);
+    setTimeout(() => {movingText.innerHTML += 'p'}, 1100);
+    setTimeout(() => {movingText.innerHTML += 'm'}, 1200);
+    setTimeout(() => {movingText.innerHTML += 'e'}, 1300);
+    setTimeout(() => {movingText.innerHTML += 'n'}, 1400);
+    setTimeout(() => {movingText.innerHTML += 't'}, 1500);
+    setTimeout(() => {movingText.innerHTML += ''}, 1600);
     setTimeout(() => {movingText.innerHTML += ''}, 1700);
     setTimeout(() => {movingText.innerHTML += ''}, 1800);
     setTimeout(() => {movingText.innerHTML += ''}, 1900);
